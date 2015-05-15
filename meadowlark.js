641242f3f4b3d4bda3b589f29ec7b30237320054
Mike <mike@veryona.com>
2015-05-15 07:21:26 -0400

    Initial commit

---
+++ meadowlark.js
@@ -0,0 +1,50 @@
+var express = require('express');
+
+var app = express();
+
+var handlebars = require('express3-handlebars')
+				.create({defaultLayout: 'main'});
+
+	app.engine('handlebars', handlebars.engine);
+	app.set('view engine', 'handlebars');
+
+var fortunes = ['e', 'f', 'd', 'a', 'b'];
+
+app.set('port', process.env.PORT || 3000);
+
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', function(req, res){
+	res.render('home');
+});
+
+app.get('/about', function(req, res){
+	//res.render('about');
+
+	var randomFortune = fortunes[Math.floor(Math.random()
+		* fortunes.length)];
+
+	res.render('about', {
+		fortune: randomFortune
+	});
+});
+
+
+app.use(function(req, res){
+	res.status(404);
+	res.render('404');
+});
+
+app.use(function(err, req, res, next){
+
+	console.error(err.stack);
+	res.status(500);
+	res.render('500');
+});
+
+
+app.listen(app.get('port'), function(){
+
+
+	console.log('App started on port:'+ app.get('port'));
+});
